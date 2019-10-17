7b63107cc5fae59a9c64ee2c343149cdb88f49e7
Infamy <infamy@unyx.io>
2019-10-17 08:37:37 -0600

    Removed getClient and cadets from client.ts for clarity

--- example/client.ts
+++ example/client.ts
@@ -1,22 +1,6 @@
 let PROTO_PATH = __dirname + '/proto/service.proto';
 let grpc = require('grpc');
 let protoLoader = require('@grpc/proto-loader');
-// Suggested options for similarity to existing grpc.load behavior
-let cadets = [
-    {
-        name: "Sears Bignose",
-        classYear: 2020
-    },
-    {
-        name: "Grady Phillips",
-        classYear: 2022
-    }
-];
-
-function getCadet(call, callback) {
-    callback(null, cadets[call.request])
-}
-
 let packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
     {
